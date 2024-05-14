05ce6d023aea85297ec1262686c0c938cfca1411
jojh5076 <jojh5075@naver.com>
2024-05-14 21:37:47 +0900

    Initialize project using Create React App

--- src/components/new.jsx
+++ src/components/new.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './new.module.css';
         
 export function New() {
+    const [showList02, setShowList02] = useState(false);
+
+    const toggleList02 = () => {
+        setShowList02(!showList02);
+    };
+
     return(
         <div className={styles.new_list_a}>
             <div className={styles.new_list}>
@@ -12,7 +18,8 @@ export function New() {
                 </div>
                 <ul className={styles.list_01}>
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39309" className={styles.showInfoBtn_ClickCount_gtm1}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39309"
+                           className={styles.showInfoBtn_ClickCount_gtm1}>
                             <img className={styles.lazyload} src="/new/303.png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -23,7 +30,8 @@ export function New() {
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39264" className={styles.showInfoBtn_ClickCount_gtm2}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39264"
+                           className={styles.showInfoBtn_ClickCount_gtm2}>
                             <img className={styles.lazyload} src="/new/303 (1).png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -31,11 +39,12 @@ export function New() {
                             <span>153,000원<span>(200장)</span>
 					  </span>
                         </div>
-                        
+
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39069" className={styles.showInfoBtn_ClickCount_gtm3}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39069"
+                           className={styles.showInfoBtn_ClickCount_gtm3}>
                             <img className={styles.lazyload} src="/new/303 (2).png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -46,7 +55,8 @@ export function New() {
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39191" className={styles.showInfoBtn_ClickCount_gtm4}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39191"
+                           className={styles.showInfoBtn_ClickCount_gtm4}>
                             <img className={styles.lazyload} src="/new/303 (3).png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -57,9 +67,10 @@ export function New() {
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=37148" className={styles.showInfoBtn_ClickCount_gtm5}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=37148"
+                           className={styles.showInfoBtn_ClickCount_gtm5}>
                             <img className={styles.lazyload} src="/new/303 (4).png" alt=""/>
-                            </a>
+                        </a>
                         <div className={styles.goods_pricing}>
                             <span className={styles.goods_bold}>꽃길로맨스 </span>
                             <span>202,400원<span>(200장)</span>
@@ -68,7 +79,8 @@ export function New() {
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39263" className={styles.showInfoBtn_ClickCount_gtm6}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39263"
+                           className={styles.showInfoBtn_ClickCount_gtm6}>
                             <img className={styles.lazyload} src="/new/303 (5).png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -79,7 +91,8 @@ export function New() {
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39170" className={styles.showInfoBtn_ClickCount_gtm7}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39170"
+                           className={styles.showInfoBtn_ClickCount_gtm7}>
                             <img className={styles.lazyload} src="/new/303 (6).png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -90,7 +103,8 @@ export function New() {
                     </li>
 
                     <li className={styles.s_thum}>
-                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39129" className={styles.showInfoBtn_ClickCount_gtm8}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39129"
+                           className={styles.showInfoBtn_ClickCount_gtm8}>
                             <img className={styles.lazyload} src="/new/303 (7).png" alt=""/>
                         </a>
                         <div className={styles.goods_pricing}>
@@ -100,102 +114,113 @@ export function New() {
                         </div>
                     </li>
                 </ul>
-                <ul className={styles.list_02}>
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39116" className={styles.showInfoBtn_ClickCount_gtm9}>
-                        <img className={styles.lazyload} src="/new/303 (8).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>The Classic </span>
-                        <span>228,800원<span>(200장)</span>
+                <ul className={`${styles.list_02} ${showList02 ? styles.active : ''}`}>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39116"
+                           className={styles.showInfoBtn_ClickCount_gtm9}>
+                            <img className={styles.lazyload} src="/new/303 (8).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>The Classic </span>
+                            <span>228,800원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39144" className={styles.showInfoBtn_ClickCount_gtm10}>
-                        <img className={styles.lazyload} src="/new/303 (9).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>꽃의 노래 </span>
-                        <span>193,600원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39144"
+                           className={styles.showInfoBtn_ClickCount_gtm10}>
+                            <img className={styles.lazyload} src="/new/303 (9).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>꽃의 노래 </span>
+                            <span>193,600원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39183" className={styles.showInfoBtn_ClickCount_gtm11}>
-                        <img className={styles.lazyload} src="/new/303 (10).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>행복한 순간 </span>
-                        <span>221,400원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39183"
+                           className={styles.showInfoBtn_ClickCount_gtm11}>
+                            <img className={styles.lazyload} src="/new/303 (10).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>행복한 순간 </span>
+                            <span>221,400원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39118" className={styles.showInfoBtn_ClickCount_gtm12}>
-                        <img className={styles.lazyload} src="/new/303 (11).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>사랑의 맹세 </span>
-                        <span>211,200원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39118"
+                           className={styles.showInfoBtn_ClickCount_gtm12}>
+                            <img className={styles.lazyload} src="/new/303 (11).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>사랑의 맹세 </span>
+                            <span>211,200원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39311" className={styles.showInfoBtn_ClickCount_gtm13}>
-                        <img className={styles.lazyload} src="/new/303 (12).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>함께인 계절 </span>
-                        <span>184,800원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39311"
+                           className={styles.showInfoBtn_ClickCount_gtm13}>
+                            <img className={styles.lazyload} src="/new/303 (12).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>함께인 계절 </span>
+                            <span>184,800원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39133" className={styles.showInfoBtn_ClickCount_gtm14}>
-                        <img className={styles.lazyload} src="/new/303 (13).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>Our Moment </span>
-                        <span>264,000원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39133"
+                           className={styles.showInfoBtn_ClickCount_gtm14}>
+                            <img className={styles.lazyload} src="/new/303 (13).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>Our Moment </span>
+                            <span>264,000원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39163" className={styles.showInfoBtn_ClickCount_gtm15}>
-                        <img className={styles.lazyload} src="/new/303 (14).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>설렘 가득히 </span>
-                        <span>155,800원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39163"
+                           className={styles.showInfoBtn_ClickCount_gtm15}>
+                            <img className={styles.lazyload} src="/new/303 (14).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>설렘 가득히 </span>
+                            <span>155,800원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
+                        </div>
+                    </li>
 
-                <li className={styles.s_thum}>
-                    <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39172" className={styles.showInfoBtn_ClickCount_gtm16}>
-                        <img className={styles.lazyload} src="/new/303 (15).png" alt=""/>
-                    </a>
-                    <div className={styles.goods_pricing}>
-                        <span className={styles.goods_bold}>웨딩파티 </span>
-                        <span>180,400원<span>(200장)</span>
+                    <li className={styles.s_thum}>
+                        <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39172"
+                           className={styles.showInfoBtn_ClickCount_gtm16}>
+                            <img className={styles.lazyload} src="/new/303 (15).png" alt=""/>
+                        </a>
+                        <div className={styles.goods_pricing}>
+                            <span className={styles.goods_bold}>웨딩파티 </span>
+                            <span>180,400원<span>(200장)</span>
 					  </span>
-                    </div>
-                </li>
-            </ul>
+                        </div>
+                    </li>
+                </ul>
             </div>
 
             <div className={styles.new_list_btn}>
-                <a href="https://www.barunsoncard.com/product/detail.asp?card_seq=39309" className={styles.btn_roll}>
-                    <img className={styles.lazyload} src="/new/btn_add_blue_off.png" alt=""/>
-                        <img className={styles.over} src="/new/btn_add_blue_on.png" alt=""/>
-                </a>
+                <button className={styles.btn_roll} onClick={toggleList02}>
+                    {showList02 ? (
+                        <img className={styles.on_over} src="/new/btn_add_blue_on.png" alt=""/>
+                    ) : (
+                        <img className={styles.off_over} src="/new/btn_add_blue_off.png" alt=""/>
+                    )}
+                </button>
             </div>
         </div>
     )
