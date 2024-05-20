410782cbeeb7643a7203e34fd0bcd24d8dd6cf33
jojh5076 <jojh5075@naver.com>
2024-05-20 13:43:00 +0900

    Initialize project using Create React App

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -8,22 +8,22 @@ export function Info() {
                 <ul className={styles.info_list}>
                     <li>
                         <a href='http://www.barunn.net/' className={styles.info_img}>
-                            <img src='/info/ban_brand_01.jpg' alt='Brand banner 1'/>
+                            <img src='/info/ban_brand_01.jpg' alt='Brand banner 1' className={styles.img1}/>
                         </a>
                     </li>
                     <li>
                         <a href='https://www.barunsoncard.com/brand/intro/brand.asp' className={styles.info_img}>
-                            <img src='/info/ban_brand_02.jpg' alt='Brand banner 2'/>
+                            <img src='/info/ban_brand_02.jpg' alt='Brand banner 2' className={styles.img2}/>
                         </a>
                     </li>
                     <li>
                         <a href='http://www.barunn.net/' className={styles.info_img}>
-                            <img src='/info/ban_brand_03.jpg' alt='Brand banner 3'/>
+                            <img src='/info/ban_brand_03.jpg' alt='Brand banner 3' className={styles.img3}/>
                         </a>
                     </li>
                     <li>
                         <a className={styles.info_img}>
-                            <img src='/info/ban_brand_04_230209.jpg' alt='Brand banner 4'/>
+                            <img src='/info/ban_brand_04_230209.jpg' alt='Brand banner 4' className={styles.img4}/>
                         </a>
                     </li>
                 </ul>
